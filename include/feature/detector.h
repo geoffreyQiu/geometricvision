f754e8e8ae16b6ede2fb372583b181b89fbf5145
Geoffrey Qiu <q8geoffrey@gmail.com>
2017-07-31 22:17:37 -0700

    create features (corners) detectors

---
+++ include/feature/detector.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+namespace gv {
+	class Detector
+	{
+	public:
+		Detector();
+		~Detector();
+		
+	};
+}
---
+++ include/feature/goodftdetector.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+namespace gv {
+	class ShiTomasiDetector : public Detector
+	{
+	public:
+		ShiTomasiDetector();
+		~ShiTomasiDetector();
+		
+	};
+}
